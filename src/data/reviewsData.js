b8a349fd2b57cfc06fa64051d93f42d94f94b0e6
adirbuskila <adirbu98@gmail.com>
2025-03-27 18:52:49 +0200

    new data

--- src/data/reviewsData.js
+++ src/data/reviewsData.js
@@ -10,8 +10,7 @@ const reviewsData = [
       taste: 9,
       cleanliness: 10,
       servingSpeed: 9,
-      saladVariety: 10,
-      // Average = (9 + 10 + 9 + 10) / 4 = 9.5
+      saladVariety: 10, // Avg: (9 + 10 + 9 + 10) / 4 = 9.5
     },
   },
   {
@@ -23,8 +22,103 @@ const reviewsData = [
       taste: 9,
       cleanliness: 8,
       servingSpeed: 9,
-      saladVariety: 10,
-      // Average = (9 + 8 + 9 + 10) / 4 = 9.0
+      saladVariety: 10, // Avg: (9 + 8 + 9 + 10) / 4 = 9.0
+    },
+  },
+  {
+    id: 3,
+    date: "2025-04-03",
+    placeName: "Shawarma Laffa",
+    location: "Tel Aviv, Israel",
+    scores: {
+      taste: 8,
+      cleanliness: 9,
+      servingSpeed: 8,
+      saladVariety: 9, // Avg: (8+9+8+9)/4 = 8.5
+    },
+  },
+  {
+    id: 4,
+    date: "2025-04-04",
+    placeName: "Shawarma Sheinkin",
+    location: "Tel Aviv, Israel",
+    scores: {
+      taste: 9,
+      cleanliness: 9,
+      servingSpeed: 9,
+      saladVariety: 9, // Avg: 9
+    },
+  },
+  {
+    id: 5,
+    date: "2025-04-05",
+    placeName: "Shawarma BaLi",
+    location: "Ashkelon, Israel",
+    scores: {
+      taste: 10,
+      cleanliness: 7,
+      servingSpeed: 8,
+      saladVariety: 9, // Avg: 8.5
+    },
+  },
+  {
+    id: 6,
+    date: "2025-04-06",
+    placeName: "Shawarma Beni",
+    location: "Rehovot, Israel",
+    scores: {
+      taste: 7,
+      cleanliness: 7,
+      servingSpeed: 7,
+      saladVariety: 7, // Avg: 7
+    },
+  },
+  {
+    id: 7,
+    date: "2025-04-07",
+    placeName: "Shawarma Achla",
+    location: "Petah Tikva, Israel",
+    scores: {
+      taste: 9,
+      cleanliness: 9,
+      servingSpeed: 10,
+      saladVariety: 10, // Avg: 9.5
+    },
+  },
+  {
+    id: 8,
+    date: "2025-04-08",
+    placeName: "Shawarma Menashe",
+    location: "Holon, Israel",
+    scores: {
+      taste: 8,
+      cleanliness: 10,
+      servingSpeed: 9,
+      saladVariety: 8, // Avg: 8.75
+    },
+  },
+  {
+    id: 9,
+    date: "2025-04-09",
+    placeName: "Shawarma Zohar",
+    location: "Bat Yam, Israel",
+    scores: {
+      taste: 9,
+      cleanliness: 8,
+      servingSpeed: 7,
+      saladVariety: 8, // Avg: 8
+    },
+  },
+  {
+    id: 10,
+    date: "2025-04-10",
+    placeName: "Shawarma Moshe",
+    location: "ראשון לציון, Israel",
+    scores: {
+      taste: 10,
+      cleanliness: 9,
+      servingSpeed: 8,
+      saladVariety: 9, // Avg: 9
     },
   },
 ];
